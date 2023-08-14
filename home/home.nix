4652afeb7c1dca3585f436e7ef4a4fc3172c6bae
Hobr <mail@hobr.site>
2023-08-14 23:20:10 +0800

    new home-manager entrypoint

---
+++ home/home.nix
@@ -0,0 +1,54 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # modules/home-manager
+    # outputs.homeManagerModules.example
+
+    # flakes
+    # inputs.nix-colors.homeManagerModules.default
+
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # flakes
+      # neovim-nightly-overlay.overlays.default
+
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "hobr";
+    homeDirectory = "/home/hobr";
+  };
+
+  # programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "23.05";
+}
