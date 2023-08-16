59925f72f4954c7d91b601426767dc0ff9af2a60
Hobr <mail@hobr.site>
2023-08-16 18:09:34 +0800

    tpm2 & luks

--- system/boot/secure.nix
+++ system/boot/secure.nix
@@ -1,6 +1,24 @@
 {
   # luks
 
-  # shim
-  # tpm
+  #boot = {
+  #  initrd = {
+  # 加密
+  #    luks.devices = {
+  #      root = {
+  #        device = "/dev/disk/by-uuid/e3188239-f080-4415-8b6a-dbcd2437cad6";
+  #        preLVM = true;
+  #      };
+  #    };
+  #  };
+  #};
+
+  security = {
+    # shim
+
+    # TPM
+    tpm2 = {
+      enable = true;
+    };
+  };
 }
