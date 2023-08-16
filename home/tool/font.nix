54b2b15fe8354550b14f1f9efd7db373d0ccae97
Hobr <mail@hobr.site>
2023-08-16 19:19:10 +0800

    fontconfig

--- home/tool/font.nix
+++ home/tool/font.nix
@@ -1,4 +1,40 @@
-{
+{pkgs, ...}: {
   # Nerd fonts
   # Noto fonts
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      source-han-sans
+      source-han-serif
+      sarasa-gothic
+      source-code-pro
+      hack-font
+      jetbrains-mono
+    ];
+  };
+
+  fontconfig = {
+    defaultFonts = {
+      emoji = ["Noto Color Emoji"];
+      monospace = [
+        "Noto Sans Mono CJK SC"
+        "Sarasa Mono SC"
+        "DejaVu Sans Mono"
+      ];
+      sansSerif = [
+        "Noto Sans CJK SC"
+        "Source Han Sans SC"
+        "DejaVu Sans"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
+        "Source Han Serif SC"
+        "DejaVu Serif"
+      ];
+    };
+    allowBitmaps = false;
+  };
 }
