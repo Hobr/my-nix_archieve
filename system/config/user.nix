5496dc56070fe723500cd6cb34c769e9938f5699
Hobr <mail@hobr.site>
2023-08-16 18:10:37 +0800

    nix allowed user

--- system/config/user.nix
+++ system/config/user.nix
@@ -5,4 +5,12 @@
       extraGroups = ["wheel" "networkmanager" "audio"];
     };
   };
+
+  nix.settings = {
+    # sudo可信
+    allowed-users = ["root" "@wheel"];
+
+    # nix可信
+    trusted-users = ["root" "@wheel"];
+  };
 }
