98d70f447c9c61d13c66caa7f3582560e628dcfb
Hobr <mail@hobr.site>
2023-08-16 18:10:43 +0800

    fstrim

--- system/hardware/base.nix
+++ system/hardware/base.nix
@@ -1,4 +1,9 @@
 {modulesPath, ...}: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+
+  # 驱动
   hardware.enableRedistributableFirmware = true;
+
+  # fstrim
+  services.fstrim.enable = true;
 }
