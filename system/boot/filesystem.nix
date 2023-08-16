227781c41c7a2cbd7a31a71db9d6e2e9694edce3
Hobr <mail@hobr.site>
2023-08-16 18:08:38 +0800

    new fstab

--- system/boot/filesystem.nix
+++ system/boot/filesystem.nix
@@ -21,5 +21,61 @@
 
   swapDevices = [];
 
-  boot.supportedFilesystems = ["ntfs"];
+  #fileSystems = {
+  # tmpfs
+  #  "/" = {
+  #    device = "none";
+  #    fsType = "tmpfs";
+  #    options = ["defaults" "mode=755" "size=6G"];
+  #  };
+
+  #  "/home/hobr" = {
+  #    device = "none";
+  #    fsType = "tmpfs";
+  #    options = ["defaults" "mode=777" "size=6G"];
+  #  };
+
+  # btrfs
+  #  "/persist" = {
+  #    device = "/dev/disk/by-uuid/9f4e2d7f-5bf7-4550-9c8f-3efb621edf05";
+  #    fsType = "btrfs";
+  #    options = ["subvol=persist" "compress=zstd" "ssd" "noatime" "nodiratime"];
+  #    neededForBoot = true;
+  #  };
+
+  #  "/nix" = {
+  #    device = "/dev/disk/by-uuid/9f4e2d7f-5bf7-4550-9c8f-3efb621edf05";
+  #    fsType = "btrfs";
+  #    options = ["subvol=nix" "compress=zstd" "ssd" "noatime" "nodiratime"];
+  #  };
+
+  # EFI
+  #  "/boot" = {
+  #    device = "/dev/disk/by-uuid/C144-3BFB";
+  #    fsType = "vfat";
+  #  };
+
+  # NTFS
+  #  "/mnt/windows" = {
+  #    device = "/dev/disk/by-uuid/28BAF5142169A897";
+  #    fsType = "ntfs-3g";
+  #  };
+
+  #  "/mnt/data" = {
+  #    device = "/dev/disk/by-uuid/5A9C7E8B6EE23C3F";
+  #    fsType = "ntfs-3g";
+  #  };
+  #};
+
+  # 交换
+  # swapDevices = [{device = "/dev/disk/by-uuid/8faba14d-01c4-4d75-9a19-7686e53d7836";}];
+
+  boot.supportedFilesystems = ["btrfs" "ntfs"];
+
+  # btrfs自动清理
+  #services.btrfs.autoScrub = {
+  #  enable = true;
+  #  interval = "weekly";
+  #  fileSystems = ["/persist" "/nix"];
+  #};
 }
