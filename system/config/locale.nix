c59cde173642a487e8603eebadeafeae28edd2f9
Hobr <mail@hobr.site>
2023-08-16 14:14:28 +0000

    timezone

--- system/config/locale.nix
+++ system/config/locale.nix
@@ -1,4 +1,10 @@
-{
+{config, ...}: {
+  time.timeZone = "Asia/Shanghai";
+  # time.hardwareClockInLocalTime = true;
   i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.supportedLocales = ["zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8"];
+
+  environment.variables = {
+    TZ = "${config.time.timeZone}";
+  };
 }
