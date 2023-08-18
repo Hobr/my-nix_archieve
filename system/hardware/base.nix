ddab46c4834c93ad077534dcba8a7a6592bfe212
Hobr <mail@hobr.site>
2023-08-18 11:45:22 +0800

    disable tlp

--- system/hardware/base.nix
+++ system/hardware/base.nix
@@ -13,17 +13,17 @@
 
     thermald.enable = true;
 
-    power-profiles-daemon.enable = false;
-    tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 0;
-        STOP_CHARGE_THRESH_BAT0 = 1;
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      };
-    };
+    power-profiles-daemon.enable = true;
+    #tlp = {
+    #  enable = true;
+    #  settings = {
+    #    START_CHARGE_THRESH_BAT0 = 0;
+    #    STOP_CHARGE_THRESH_BAT0 = 1;
+    #    CPU_BOOST_ON_AC = 1;
+    #    CPU_BOOST_ON_BAT = 0;
+    #    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    #  };
+    #};
   };
 }
