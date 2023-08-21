0d1a64ef21c9c72f3f2a32077245b32674567530
Hobr <mail@hobr.site>
2023-08-21 13:57:43 +0800

    fcitx5 zhwiki moegirl

--- home/tool/fcitx.nix
+++ home/tool/fcitx.nix
@@ -1,14 +1,16 @@
 {pkgs, ...}: {
   # Fcitx5
   # Fcitx5-rime
+  # fcitx-moegirl
+  # fcitx-zhwiki
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-rime
       fcitx5-gtk
+      fcitx5-pinyin-zhwiki
+      fcitx5-pinyin-moegirl
     ];
   };
   # rime-ice
-  # fcitx-moegirl
-  # fcitx-zhwiki
 }
