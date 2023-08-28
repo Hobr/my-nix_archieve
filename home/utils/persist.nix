5fb4a3c6b6f202b5f4ae7c09dfb783dbd2403e7d
Hobr <mail@hobr.site>
2023-08-28 12:28:17 +0800

    persist

---
+++ home/utils/persist.nix
@@ -0,0 +1,13 @@
+{
+  # impermanence
+  home.persistence."/persist/home/hobr" = {
+    removePrefixDirectory = true;
+    allowOther = true;
+
+    directories = [
+    ];
+
+    files = [
+    ];
+  };
+}
--- system/boot/persist.nix
+++ system/boot/persist.nix
@@ -1,3 +1,12 @@
 {
   # impermanence
+  environment.persistence."/persist" = {
+    hideMounts = true;
+
+    directories = [
+    ];
+
+    files = [
+    ];
+  };
 }
