586184541a0e2b74ae142156d954c93320e96290
Hobr <mail@hobr.site>
2023-08-28 20:54:19 +0800

    splash

--- system/boot/splash.nix
+++ system/boot/splash.nix
@@ -1,4 +1,11 @@
 {
   # plymouth
+  boot.plymouth.enable = true;
+
   # lightdm
+  #services.xserver.displayManager.lightdm = {
+  #  enable = true;
+  #};
+
+  boot.initrd.systemd.enable = true;
 }
