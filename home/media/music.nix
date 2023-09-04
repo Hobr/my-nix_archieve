1e5b00fddf06c26b434ad0a814c8fc769008f05b
Hobr <mail@hobr.site>
2023-09-04 13:13:50 +0800

    audio

--- home/media/music.nix
+++ home/media/music.nix
@@ -1,5 +1,20 @@
-{
+{pkgs, ...}: {
   # mpd
+  services.mpd = {
+    enable = true;
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "My PipeWire Output"
+      }
+    '';
+  };
+
   # ncmpcpp
+  programs.ncmpcpp = {
+    enable = true;
+  };
+
   # neteats-cloud-music-gtk
+  home.packages = [pkgs.netease-cloud-music-gtk];
 }
