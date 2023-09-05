f2557b5f97455b1a6b1f933e06e9a16230ba3d7b
Hobr <mail@hobr.site>
2023-09-05 16:07:07 +0800

    hyprland

--- home/desktop/hypr.nix
+++ home/desktop/hypr.nix
@@ -1,11 +1,13 @@
-{
+{pkgs, ...}: {
   # Hyprland
-  #wayland.windowManager.hyprland = {
-  #  enable = true;
-  #  xwayland.enable = true;
-  #  enableNvidiaPatches = true;
-  #s};
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    enableNvidiaPatches = true;
+  };
+
   # Hyprpaper
+  home.packages = [pkgs.hyprpaper];
   # Hyprload
   ## Hypr-empty
   ## hyprfocus
